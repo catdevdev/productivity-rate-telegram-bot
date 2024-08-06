2e1c47066fac74f1239bbb90857254d08cb30dd8
Vladislav Oliinyk <catdev.dev@gmail.com>
2024-08-07 01:36:53 +0300

    fix

--- bot/main.py
+++ bot/main.py
@@ -85,7 +85,7 @@ async def calculate(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
                     accesswebcam();"
 
                     все ответы должны быть на русском языке
-                    ответы должны быть в виде 3-6 пунктов в виде инстукции
+                    ответы должны быть в виде 3-6 пунктов в виде инстукции. пукнт по 10-12 предложений
                     """ + expression,
                 }
             ],
